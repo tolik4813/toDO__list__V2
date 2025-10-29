4b1879bc7ea60d89e68d6457aa66035b4d4bc946
tolik4813 <afikusovich@gmail.com>
2025-10-30 00:47:47 +0200

    refactor(virtual-list): remove VirtualList component and threshold logic

--- app/features/todo/index.ts
+++ app/features/todo/index.ts
@@ -3,4 +3,3 @@ export { default as TodoItem } from '@/app/customComponents/todo/TodoItem';
 export { default as TasksList } from '@/app/customComponents/todo/TasksList';
 export { default as TodoActions } from '@/app/customComponents/todo/TodoActions';
 export { default as TodoContent } from '@/app/customComponents/todo/TodoContent';
-export { default as VirtualList } from '@/app/customComponents/todo/VirtualList';
